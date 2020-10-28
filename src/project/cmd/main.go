caf6cc38f5624398475cd4e0210203b203ad5ffd
Haris Chaniotakis <chanioxaris@gmail.com>
2020-10-28 22:47:28 +0200

    Completed 1.12

--- src/project/cmd/main.go
+++ src/project/cmd/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 )
 
 func main() {
+	fileServer := http.FileServer(http.Dir("/usr/src/app/files"))
+
 	http.HandleFunc("/", indexPageHandler())
+	http.Handle("/usr/src/app/files/", http.StripPrefix("/usr/src/app/files", fileServer))
 
 	wg := &sync.WaitGroup{}
 
@@ -17,22 +22,31 @@ func main() {
 	go func() {
 		defer wg.Done()
 
-		log.Fatal(http.ListenAndServe(":8080", nil))
+		if err := http.ListenAndServe(":3000", nil); err != nil {
+			log.Fatal(err)
+		}
 	}()
 
-	fmt.Println("Server started in port 8080")
+	fmt.Println("Server started in port 3000")
 
 	wg.Wait()
 }
 
 const indexPage = `
 <html>
-  <h1>Hello from project server</h1>
+	<h1>Hello from project server</h1>
+
+	<img src="/usr/src/app/files/image.jpg" width="500" height="500"/>
 </html>
 `
 
 func indexPageHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if err := downloadImage(); err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
 		t, err := template.New("foo").Parse(indexPage)
 		if err != nil {
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
@@ -44,4 +58,38 @@ func indexPageHandler() http.HandlerFunc {
 			return
 		}
 	}
+}
+
+func downloadImage() error {
+	if fileExists("/usr/src/app/files/image.jpg") {
+		return nil
+	}
+
+	response, err := http.Get("https://picsum.photos/1200")
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	f, err := os.Create("/usr/src/app/files/image.jpg")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err = io.Copy(f, response.Body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fileExists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+
+	return true
 }
